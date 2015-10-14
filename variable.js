768ac24449acab3f80da61fe4b461b46b2ee651b
Erik Wetterberg <ewg@qlik.com>
2015-10-14 09:24:55 +0200

    Modifications for 2.1
    Check if variable already exists to avoid error message

--- variable.js
+++ variable.js
@@ -1,73 +1,90 @@
-define(["qlik"], function(qlik) {
-	var BTN_SELECTED = 'qui-button-selected qirby-button-selected', BTN = 'qui-button qirby-button', SELECT = 'qui-select qirby-select', INPUT = 'qui-input qirby-input';
+/*global define*/
+define( ["qlik"], function ( qlik ) {
+	'use strict';
+	var BTN_SELECTED = 'qui-button-selected', BTN = 'qui-button', SELECT = 'qui-select', INPUT = 'qui-input';
+
+	function createVariable ( name ) {
+		var app = qlik.currApp();
+		//from 2.1: check if variable exists
+		if ( app.variable.getByName ) {
+			app.variable.getByName( name ).then( function () {
+				//variable already exist
+			}, function () {
+				//create variable
+				app.variable.create( name );
+			} );
+		} else {
+			//create variable - ignore errors
+			app.variable.create( name );
+		}
+	}
 
 	return {
-		initialProperties : {
-			version : 1.0,
-			variableValue : {},
-			variableName : "",
-			render : "f",
-			alternatives : []
+		initialProperties: {
+			version: "2.0.0",
+			variableValue: {},
+			variableName: "",
+			render: "f",
+			alternatives: []
 		},
-		definition : {
-			type : "items",
-			component : "accordion",
-			items : {
-				settings : {
-					uses : "settings",
-					items : {
-						variable : {
-							type : "items",
-							label : "Variable",
-							items : {
-								name : {
-									ref : "variableName",
-									label : "Name",
-									type : "string",
-									change : function(data) {
-										//create variable - ignore errors
-										qlik.currApp().variable.create(data.variableName);
+		definition: {
+			type: "items",
+			component: "accordion",
+			items: {
+				settings: {
+					uses: "settings",
+					items: {
+						variable: {
+							type: "items",
+							label: "Variable",
+							items: {
+								name: {
+									ref: "variableName",
+									label: "Name",
+									type: "string",
+									change: function ( data ) {
+										createVariable( data.variableName );
 										data.variableValue.qStringExpression = '=' + data.variableName;
 									}
 								},
-								render : {
-									type : "string",
-									component : "dropdown",
-									label : "Render as",
-									ref : "render",
-									options : [{
-										value : "b",
-										label : "Button"
+								render: {
+									type: "string",
+									component: "dropdown",
+									label: "Render as",
+									ref: "render",
+									options: [{
+										value: "b",
+										label: "Button"
 									}, {
-										value : "s",
-										label : "Select"
+										value: "s",
+										label: "Select"
 									}, {
-										value : "f",
-										label : "Field"
+										value: "f",
+										label: "Field"
 									}, {
-										value : "l",
-										label : "Slider"
+										value: "l",
+										label: "Slider"
 									}],
-									defaultValue : "f"
+									defaultValue: "f"
 								},
-								alternatives : {
-									type : "array",
-									ref : "alternatives",
-									label : "Alternatives",
-									itemTitleRef : "label",
-									allowAdd : true,
-									allowRemove : true,
-									addTranslation : "Add Alternative",
-									items : {
-										value : {
-											type : "string",
-											ref : "value",
-											label : "Value"
+								alternatives: {
+									type: "array",
+									ref: "alternatives",
+									label: "Alternatives",
+									itemTitleRef: "label",
+									allowAdd: true,
+									allowRemove: true,
+									addTranslation: "Add Alternative",
+									items: {
+										value: {
+											type: "string",
+											ref: "value",
+											label: "Value"
 										},
-										label : {
-											type : "string",
-											ref : "label",
-											label : "Label"
+										label: {
+											type: "string",
+											ref: "label",
+											label: "Label"
 										}
 									}
 								}
@@ -77,34 +94,35 @@ define(["qlik"], function(qlik) {
 				}
 			}
 		},
-		paint : function($element, layout) {
+		paint: function ( $element, layout ) {
 			var html = "", ext = this;
-			if(layout.render === 'b') {
-				layout.alternatives.forEach(function(alt) {
+			if ( layout.render === 'b' ) {
+				layout.alternatives.forEach( function ( alt ) {
 					var clazz = alt.value === layout.variableValue ? BTN_SELECTED : BTN;
 					html += "<button class='" + clazz + "' data-alt='" + alt.value + "'>" + alt.label + "</button>";
-				});
-			} else if(layout.render === 's') {
+				} );
+			} else if ( layout.render === 's' ) {
 				html += '<select class="' + SELECT + '">';
-				layout.alternatives.forEach(function(alt) {
+				layout.alternatives.forEach( function ( alt ) {
 					var sel = alt.value === layout.variableValue ? 'selected' : '';
 					html += "<option value='" + alt.value + "' " + sel + " >" + alt.label + "</option>";
-				});
+				} );
 				html += '</select>';
-			} else if(layout.render === 'l') {
+			} else if ( layout.render === 'l' ) {
 				html += '<input type="range" min="0" max="100" style="width:98%" value="' + layout.variableValue + '"/>';
 			} else {
-				html += '<input class="' + INPUT + '" type="text"  value="' + layout.variableValue + '" />';
+				html += '<input class="' + INPUT + '" type="text"  value="' + (typeof layout.variableValue === "string" ? layout.variableValue : "") + '" />';
 			}
-			$element.html(html).find('button').on('qv-activate', function() {
-				var val = $(this).data('alt') + '';
-				qlik.currApp(ext).variable.setContent(layout.variableName, val);
-			});
-			$element.find('select, input').on('change', function() {
-				var val = $(this).val() + '';
-				qlik.currApp(ext).variable.setContent(layout.variableName, val);
-			})
+			$element.html( html ).find( 'button' ).on( 'qv-activate', function () {
+				var val = $( this ).data( 'alt' ) + '';
+				qlik.currApp( ext ).variable.setContent( layout.variableName, val );
+			} );
+			$element.find( 'select, input' ).on( 'change', function () {
+				var val = $( this ).val() + '';
+				qlik.currApp( ext ).variable.setContent( layout.variableName, val );
+			} )
 		}
 	};
 
-});
+} )
+;
